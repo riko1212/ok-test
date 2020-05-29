85084eaf61a82d6928e3a4b5e396885d1d8f9039
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2020-05-29 15:33:03 +0300

    Add files via upload

---
+++ js/geo.js
@@ -0,0 +1,30 @@
+function geoplugin_request() { return '37.115.189.14';}
+function geoplugin_status() { return '200';}
+function geoplugin_credit() { return 'Some of the returned data includes GeoLite data created by MaxMind, available from <a href=\'http://www.maxmind.com\'>http://www.maxmind.com</a>.';}
+function geoplugin_delay() { return '1ms';}
+function geoplugin_city() { return 'Lviv';}
+function geoplugin_region() { return 'L\'vivs\'ka Oblast\'';}
+function geoplugin_regionCode() { return '46';}
+function geoplugin_regionName() { return 'L\'vivs\'ka Oblast\'';}
+function geoplugin_areaCode() { return '';}
+function geoplugin_dmaCode() { return '';}
+function geoplugin_countryCode() { return 'UA';}
+function geoplugin_countryName() { return 'Ukraine';}
+function geoplugin_inEU() { return 0;}
+function geoplugin_euVATrate() { return ;}
+function geoplugin_continentCode() { return 'EU';}
+function geoplugin_latitude() { return '49.8486';}
+function geoplugin_longitude() { return '24.0323';}
+function geoplugin_locationAccuracyRadius() { return '10';}
+function geoplugin_timezone() { return 'Europe/Kiev';}
+function geoplugin_currencyCode() { return 'UAH';}
+function geoplugin_currencySymbol() { return '&#8372;';}
+function geoplugin_currencySymbol_UTF8() { return '₴';}
+function geoplugin_currencyConverter(amt, symbol) {
+    if (!amt) { return false; }
+    var converted = amt * 26.8981;
+    if (converted <0) { return false; }
+    if (symbol === false) { return Math.round(converted * 100)/100; }
+    else { return '&#8372;'+(Math.round(converted * 100)/100);}
+    return false;
+}
---
+++ js/main.js
@@ -0,0 +1,74 @@
+$(document).ready(
+    function(){
+        $('a').attr( 'href', 'https://www.managecarthealth.com/biox/kt/?AFFID=45&C1=1908-2381&C2=-------------&C3=10492715&C4=&click_id=9344944');
+        $('a').attr( 'target', '_blank');
+    });
+$(document).ready(function(){
+});
+
+var city = geoplugin_city();
+
+if (city == '') {
+    city = "American";
+};
+
+var state = geoplugin_region();
+if (state == '') {
+    state = "Arizona";
+};
+
+
+
+function convert_state(name, to) {
+    var name = name.toUpperCase();
+    var states = new Array(                         {'name':'Alabama', 'abbrev':'AL'},          {'name':'Alaska', 'abbrev':'AK'},
+        {'name':'Arizona', 'abbrev':'AZ'},          {'name':'Arkansas', 'abbrev':'AR'},         {'name':'California', 'abbrev':'CA'},
+        {'name':'Colorado', 'abbrev':'CO'},         {'name':'Connecticut', 'abbrev':'CT'},      {'name':'Delaware', 'abbrev':'DE'},
+        {'name':'Florida', 'abbrev':'FL'},          {'name':'Georgia', 'abbrev':'GA'},          {'name':'Hawaii', 'abbrev':'HI'},
+        {'name':'Idaho', 'abbrev':'ID'},            {'name':'Illinois', 'abbrev':'IL'},         {'name':'Indiana', 'abbrev':'IN'},
+        {'name':'Iowa', 'abbrev':'IA'},             {'name':'Kansas', 'abbrev':'KS'},           {'name':'Kentucky', 'abbrev':'KY'},
+        {'name':'Louisiana', 'abbrev':'LA'},        {'name':'Maine', 'abbrev':'ME'},            {'name':'Maryland', 'abbrev':'MD'},
+        {'name':'Massachusetts', 'abbrev':'MA'},    {'name':'Michigan', 'abbrev':'MI'},         {'name':'Minnesota', 'abbrev':'MN'},
+        {'name':'Mississippi', 'abbrev':'MS'},      {'name':'Missouri', 'abbrev':'MO'},         {'name':'Montana', 'abbrev':'MT'},
+        {'name':'Nebraska', 'abbrev':'NE'},         {'name':'Nevada', 'abbrev':'NV'},           {'name':'New Hampshire', 'abbrev':'NH'},
+        {'name':'New Jersey', 'abbrev':'NJ'},       {'name':'New Mexico', 'abbrev':'NM'},       {'name':'New York', 'abbrev':'NY'},
+        {'name':'North Carolina', 'abbrev':'NC'},   {'name':'North Dakota', 'abbrev':'ND'},     {'name':'Ohio', 'abbrev':'OH'},
+        {'name':'Oklahoma', 'abbrev':'OK'},         {'name':'Oregon', 'abbrev':'OR'},           {'name':'Pennsylvania', 'abbrev':'PA'},
+        {'name':'Rhode Island', 'abbrev':'RI'},     {'name':'South Carolina', 'abbrev':'SC'},   {'name':'South Dakota', 'abbrev':'SD'},
+        {'name':'Tennessee', 'abbrev':'TN'},        {'name':'Texas', 'abbrev':'TX'},            {'name':'Utah', 'abbrev':'UT'},
+        {'name':'Vermont', 'abbrev':'VT'},          {'name':'Virginia', 'abbrev':'VA'},         {'name':'Washington', 'abbrev':'WA'},
+        {'name':'West Virginia', 'abbrev':'WV'},    {'name':'Wisconsin', 'abbrev':'WI'},        {'name':'Wyoming', 'abbrev':'WY'}
+    );
+    var returnthis = false;
+    $.each(states, function(index, value){
+        if (to == 'name') {
+            if (value.abbrev == name){
+                returnthis = value.name;
+                return false;
+            }
+        } else if (to == 'abbrev') {
+            if (value.name.toUpperCase() == name){
+                returnthis = value.abbrev;
+                return false;
+            }
+        }
+    });
+    return returnthis;
+}
+
+var FullState = convert_state(state, "name");
+
+$( document ).ready(function() {
+    $(".state").html(FullState);
+    $(".city").html(city);
+});
+
+function today() {
+    var mydate = new Date();
+    var dayarray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var montharray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    document.write("" + dayarray[mydate.getDay()] + ", " + montharray[mydate.getMonth()] + " "
+        + mydate.getDate() + ", " + mydate.getFullYear() );
+}
+
